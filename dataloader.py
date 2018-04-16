37ac49f8226f1533021c6f5bcbc5b9ed3fcea789
Jonas Cheng <jonas.cheng@gmail.com>
2018-04-16 22:51:44 +0800

    refactor code to separate dataloader and visulizer, also support hyperas

---
+++ dataloader.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+# Importing the libraries
+import copy
+import numpy as np
+import pandas as pd
+
+# Spliting dataset into training and testing sets
+from sklearn.model_selection import train_test_split
+
+
+class DataLoader():
+    def __init__(self, filepath, normalize=True):
+        # Tuned params and variables
+        # number of sequence data
+        self.look_back = 60
+        self.normalize = normalize
+        # Importing the dataset
+        self.history = pd.read_csv(filepath, encoding='big5-hkscs')
+        print('historical data is loaded from {}'.format(filepath))
+
+    def __normalize_windows(self, window_data):
+        if self.normalize is True:
+            # Normalize dataset to improve the convergence
+            # Normalize each value to reflect the percentage changes from starting point
+            normalized_data = []
+            for window in window_data:
+                normalized_window = [((float(p) / float(window[0])) - 1) for p in window]
+                normalized_data.append(normalized_window)            
+            window_data = normalized_data
+        return window_data
+
+    def __train_test_split(self, Xy, last_5=True):
+        # Spliting dataset into training and testing sets    
+        if last_5 is True:
+            # Historical price for regression
+            X = Xy[:, :-1]
+            # Target price for regression
+            y = Xy[:, -1:]
+            # Select the last 5 working date for testing and the others for training.
+            X_train = X[:-5, :]
+            y_train = y[:-5, :]
+            X_test = X[-5:, :]
+            y_test = y[-5:, :]
+        else:
+            X = Xy[:, :-1]
+            y = Xy[:, -1:]
+            # Select 10% of the data for testing and 90% for training.
+            # Shuffle the data in order to train in random order.
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True, random_state=0) 
+
+        # Reshape the inputs from 1 dimenstion to 3 dimension
+        # X_train.shape[0]: batch_size which is number of observations
+        # X_train.shape[1]: timesteps which is look_back
+        # 1: input_dim which is number of predictors
+        X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+        X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+        return X_train, X_test, y_train, y_test
+
+    def __data(self, stock_id, last_5=True):
+        # Extracting/Filtering the training dataset by stock_id
+        dataset = self.history.loc[self.history['代碼'] == stock_id]
+        # Taking 收盤價 as a predictor
+        dataset = dataset.iloc[:, 6:7].values
+        # Composing time sequence dataset with timesteps + target
+        timesequence = []
+        for i in range(self.look_back, len(dataset)):
+            timesequence.append(dataset[i - self.look_back:i + 1, 0])
+        # Normalize dataset if needed
+        ori_Xy = copy.deepcopy(timesequence)
+        Xy = self.__normalize_windows(timesequence)
+        # Converting array of list to numpy array
+        ori_Xy = np.array(ori_Xy)
+        Xy = np.array(Xy)
+        # Spliting dataset into training and testing sets
+        self.X_ori_train, self.X_ori_test, self.y_ori_train, self.y_ori_test = self.__train_test_split(ori_Xy, last_5)
+        X_train, X_test, y_train, y_test = self.__train_test_split(Xy, last_5)
+
+        return X_train, y_train, X_test, y_test
+
+    def data_last_5_for_test(self, stock_id):
+        return self.__data(stock_id, last_5=True)
+
+    def data(self, stock_id):
+        return self.__data(stock_id, last_5=False)
+
+    def ori_data(self):
+        return self.X_ori_train, self.y_ori_train, self.X_ori_test, self.y_ori_test
+
+    def inverse_transform_prediction(self, prediction):
+        if self.normalize is True:
+            prediction = (prediction + 1) * self.X_ori_test[:, 0]
+        return prediction
---
+++ rnn_visualizing.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Importing the libraries
+from keras.models import load_model
+
+from dataloader import DataLoader
+from visualizer import Visualizer
+
+loader = DataLoader('TBrain_Round2_DataSet_20180331/tetfp.csv', normalize=True)
+
+X_train, y_train, X_test, y_test = loader.data(50)
+X_ori_train, y_ori_train, X_ori_test, y_ori_test = loader.ori_data()
+
+regressor = load_model('rnn_etf_50.h5')
+
+# Normalized prediction
+real_price = y_test
+predicted_price = regressor.predict(X_test)
+Visualizer.show(real_price, predicted_price, 'Normalized Stock Price Prediction')
+
+# Inversed transform prediction
+real_price = y_ori_test
+predicted_price = loader.inverse_transform_prediction(predicted_price)
+Visualizer.show(real_price, predicted_price, 'Stock Price Prediction')
