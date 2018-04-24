96d3ae919d140d948a865979deb08e87e3cb3c27
Jonas Cheng <jonas.cheng@gmail.com>
2018-04-24 19:40:29 +0800

    use normalize_windows instead of percent change

--- playground_stateful.py
+++ playground_stateful.py
@@ -33,31 +33,39 @@ plot_stock_price(dataset, last_ndays=240)
 #plot_stock_price(df.values, last_ndays=240)
 
 # Feature Scaling
-from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
-scaled_price = sc.fit_transform(dataset)
+#from sklearn.preprocessing import MinMaxScaler
+#sc = MinMaxScaler(feature_range = (0, 1))
+#scaled_price = sc.fit_transform(dataset)
 
 ###########################################################
 # Visualising the stock price
-from util import plot_stock_price
-plot_stock_price(dataset, last_ndays=240)
-plot_stock_price(scaled_price, last_ndays=240)
+#from util import plot_stock_price
+#plot_stock_price(dataset, last_ndays=240)
+#plot_stock_price(scaled_price, last_ndays=240)
 
 ###########################################################
 # series_to_supervised
 from util import series_to_supervised
 supervised = series_to_supervised(dataset, n_in=5, n_out=5)
-supervised2 = series_to_supervised(scaled_price, n_in=5, n_out=5)
+#supervised2 = series_to_supervised(scaled_price, n_in=5, n_out=5)
 
 ###########################################################
 # Visualising the stock price
 import copy
 from util import plot_stock_price
+plot_stock_price(dataset, first_ndays=10)
 plot_stock_price(supervised[0].transpose())
-plot_stock_price(supervised2[0].transpose())
+#plot_stock_price(supervised2[0].transpose())
+#ori_Xy = copy.deepcopy(supervised)
+#Xy = np.array(supervised2)
 
+###########################################################
+# normalize_windows
+import copy
+from util import normalize_windows
 ori_Xy = copy.deepcopy(supervised)
-Xy = np.array(supervised2)
+Xy = normalize_windows(supervised)
+
 
 ###########################################################
 # Visualising the stock price
@@ -67,7 +75,7 @@ plot_stock_price(Xy[0].transpose())
 ###########################################################
 # train_test_split
 from util import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(Xy, test_samples=1, num_forecasts=5)
+X_train, X_test, y_train, y_test = train_test_split(Xy, test_samples=240, num_forecasts=5)
 
 y_train = np.reshape(y_train, (y_train.shape[0], y_train.shape[1], 1))
 y_test = np.reshape(y_test, (y_test.shape[0], y_test.shape[1], 1))
@@ -75,16 +83,16 @@ y_test = np.reshape(y_test, (y_test.shape[0], y_test.shape[1], 1))
 ###########################################################
 # Visualising the stock price
 from util import plot_stock_price
-plot_stock_price(supervised2[0].reshape(10, 1), first_ndays=5)
+plot_stock_price(supervised[0].reshape(10, 1), first_ndays=5)
 plot_stock_price(X_train[0])
 
-plot_stock_price(supervised2[0].reshape(10, 1), last_ndays=5)
+plot_stock_price(supervised[0].reshape(10, 1), last_ndays=5)
 plot_stock_price(y_train[0])
 
-plot_stock_price(supervised2[-1].reshape(10, 1), first_ndays=5)
+plot_stock_price(supervised[-1].reshape(10, 1), first_ndays=5)
 plot_stock_price(X_test[-1])
 
-plot_stock_price(supervised2[-1].reshape(10, 1), last_ndays=5)
+plot_stock_price(supervised[-1].reshape(10, 1), last_ndays=5)
 plot_stock_price(y_test[-1])
 
 ###########################################################
@@ -104,12 +112,27 @@ regressor.add(
     batch_input_shape=(1, 5, 1),
     stateful=True))
 regressor.add(Dropout(0.2))
+
 # Adding a second LSTM layer and some Dropout regularisation
-regressor.add(
-    LSTM(units=50, 
-    return_sequences = True,
-    stateful=True))
-regressor.add(Dropout(0.2))
+#regressor.add(
+#    LSTM(units=50, 
+#    return_sequences = True,
+#    stateful=True))
+#regressor.add(Dropout(0.2))
+
+# Adding a third LSTM layer and some Dropout regularisation
+#regressor.add(
+#    LSTM(units=50, 
+#    return_sequences = True,
+#    stateful=True))
+#regressor.add(Dropout(0.2))
+
+# Adding a fourth LSTM layer and some Dropout regularisation
+#regressor.add(
+#    LSTM(units=50, 
+#    return_sequences = True,
+#    stateful=True))
+#regressor.add(Dropout(0.2))
 
 regressor.add(
     TimeDistributed(Dense(1)))
@@ -143,8 +166,7 @@ for i in range(10):
     
     real_price = np.concatenate((real_price[0], np.array(real_price)[1:, -1]))
     predicted_price = np.concatenate((predicted_price[0], np.array(predicted_price)[1:, -1]))
-    
-    
+            
     plt.plot(predicted_price, color = 'blue', label = 'Predicted Price')
     plt.plot(real_price, color = 'red', label = 'Real Price')
     plt.title('Price Prediction')
