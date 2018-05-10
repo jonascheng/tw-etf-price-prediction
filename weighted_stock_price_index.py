1acf48bf61276f5b9b8161ee2c05d89d56c46c7e
Jonas Cheng <jonas.cheng@gmail.com>
2018-05-10 21:25:10 +0800

    crawler for weighted stock price index

---
+++ weighted_stock_price_index.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import time
+import arrow
+import requests
+
+from fake_useragent import UserAgent
+
+# Request
+# http://www.twse.com.tw/indicesReport/MI_5MINS_HIST?response=json&date=20180501
+#
+# Response
+# {
+#   stat: "OK",
+#   data[["107/05/02", "open", "high", "low", "close"],...]
+# }
+with open('weighted_stock_price_index.csv', 'w') as file:
+    ua = UserAgent()
+
+    # header
+    file.write('data,open,high,low,close\n')
+
+    start_date = arrow.get(2013, 1, 1)
+    end_date = arrow.now()
+
+    for d in arrow.Arrow.range('month', start_date, end_date):
+        date = d.format('YYYYMMDD')
+        print('quering {}'.format(date))
+
+        url = 'http://www.twse.com.tw/indicesReport/MI_5MINS_HIST?response=json&date={}&_=1525951655386'.format(date)
+        headers = {'User-Agent': ua.random}
+        r = requests.get(url, headers=headers)
+
+        if r.status_code != requests.codes.ok:
+            print('error response {}'.format(r))
+            exit()
+
+        # convert to JSON object
+        r = r.json()
+        stat = r.get('stat')
+        if stat == 'OK':
+            data = r.get('data')
+            for row in data:
+                date, open, high, low, close = row
+                file.write('{},{},{},{},{}\n'.format(date, open, high, low, close))
+        else:
+            print('stat({}) is not OK'.format(stat))
+        
+        time.sleep(10)
