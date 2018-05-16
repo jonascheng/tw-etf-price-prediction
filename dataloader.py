1a0a640dcfb47fb8350b8eca35455db7934437e3
Jonas Cheng <jonas.cheng@gmail.com>
2018-05-16 21:40:52 +0800

    DataForStatelessModelWeighted

--- dataloader.py
+++ dataloader.py
@@ -6,19 +6,26 @@ import numpy as np
 import pandas as pd
 
 import abc
+from abc import abstractmethod
 from sklearn.preprocessing import MinMaxScaler
 
 import settings
-from util import load_csv, series_to_supervised, normalize_windows, train_test_split, predict_split
+from util import load_csv, load_weighted_csv
+from util import series_to_supervised, normalize_windows, train_test_split, predict_split
 from util import query_close_price, query_open_price, query_volume, query_high_price, query_low_price
+from util import query_weighted_open_price, query_weighted_close_price, query_weighted_high_price, query_weighted_low_price, query_weighted_avg_price
 from util import query_avg_price
 
 
 class DataLoader(abc.ABC):
-    def __init__(self):
+    def __init__(self, stock_id=None):
+        self.stock_id = stock_id
         # Importing the dataset
         filepath = '{}/tetfp.csv'.format(settings.DATASET_PATH)
-        self.history = load_csv(filepath)
+        self.history = load_csv(filepath, stock_id)
+        if self.stock_id is not None:
+            filepath = '{}/weighted_stock_price_index.csv'.format(settings.DATASET_PATH)
+            self.weighted_history = load_weighted_csv(filepath, self.history)
 
 
 class DataForStatelessModel(DataLoader):
@@ -93,10 +100,10 @@ class DataForStatelessModelMoreFeatures(DataLoader):
     look_back = 50
     look_forward = 5
 
-    def __init__(self):
-        super(DataForStatelessModelMoreFeatures, self).__init__()
+    def __init__(self, stock_id=None):
+        super(DataForStatelessModelMoreFeatures, self).__init__(stock_id)
 
-    def __set_look_back(self, stock_id):        
+    def _set_look_back(self, stock_id):        
         if stock_id in [57, 58]:
             self.look_back = 110
         elif stock_id in [50, 6203, 6204]:
@@ -119,7 +126,7 @@ class DataForStatelessModelMoreFeatures(DataLoader):
         print('set look back to {} for stock {}'.format(self.look_back, stock_id))
 
     def __data(self, stock_id, ndays):
-        self.__set_look_back(stock_id)
+        self._set_look_back(stock_id)
 
         # Taking 收盤價 開盤價 高低均價 成交量 as a predictor
         dataset = query_close_price(self.history, stock_id)
@@ -178,7 +185,7 @@ class DataForStatelessModelMoreFeatures(DataLoader):
         return dataset[-1:]
 
     def data_for_prediction(self, stock_id):
-        self.__set_look_back(stock_id)
+        self._set_look_back(stock_id)
 
         # Taking 收盤價 開盤價 高低均價 成交量 as a predictor
         dataset = query_close_price(self.history, stock_id)
@@ -223,6 +230,190 @@ class DataForStatelessModelMoreFeatures(DataLoader):
         X_test = np.append(X_test, feature_low_test, axis=2)
         X_test = np.append(X_test, feature_avg_test, axis=2)
         X_test = np.append(X_test, feature_vol_test, axis=2)
+
+        return self.X_ori_test, X_test
+
+    def data_last_ndays_for_test(self, stock_id, ndays):
+        return self.__data(stock_id, ndays)
+
+    def data(self, stock_id):
+        return self.__data(stock_id, ndays=0)
+
+    def ori_train_data(self):
+        return self.X_ori_train, self.y_ori_train
+
+    def ori_test_data(self):
+        return self.X_ori_test, self.y_ori_test
+
+    def inverse_transform_prediction(self, prediction):
+        prediction = (prediction + 1) * self.X_ori_test[:, 0]
+        return prediction
+
+
+class DataForStatelessModelWeighted(DataForStatelessModelMoreFeatures):
+    def __init__(self, stock_id):
+        super(DataForStatelessModelWeighted, self).__init__(stock_id)
+
+    def __data(self, stock_id, ndays):
+        super(DataForStatelessModelWeighted, self)._set_look_back(stock_id)
+
+        # Taking 收盤價 開盤價 高低均價 成交量 as a predictor
+        dataset = query_close_price(self.history, stock_id)
+        dataset_open = query_open_price(self.history, int(stock_id))
+        dataset_high = query_high_price(self.history, int(stock_id))
+        dataset_low = query_low_price(self.history, int(stock_id))
+        dataset_avg = query_avg_price(self.history, int(stock_id))
+        dataset_vol = query_volume(self.history, int(stock_id))
+        dataset_weighted_close = query_weighted_close_price(self.weighted_history)
+        dataset_weighted_open = query_weighted_open_price(self.weighted_history)
+        dataset_weighted_high = query_weighted_high_price(self.weighted_history)
+        dataset_weighted_low = query_weighted_low_price(self.weighted_history)
+        dataset_weighted_avg = query_weighted_avg_price(self.weighted_history)
+
+        # Transforming time series dataset into supervised dataset
+        supervised = series_to_supervised(dataset, n_in=self.look_back, n_out=self.look_forward)
+        supervised_open = series_to_supervised(dataset_open, n_in=self.look_back, n_out=self.look_forward)
+        supervised_high = series_to_supervised(dataset_high, n_in=self.look_back, n_out=self.look_forward)
+        supervised_low = series_to_supervised(dataset_low, n_in=self.look_back, n_out=self.look_forward)
+        supervised_avg = series_to_supervised(dataset_avg, n_in=self.look_back, n_out=self.look_forward)
+        supervised_vol = series_to_supervised(dataset_vol, n_in=self.look_back, n_out=self.look_forward)
+        supervised_weighted_close = series_to_supervised(dataset_weighted_close, n_in=self.look_back, n_out=self.look_forward)
+        supervised_weighted_open = series_to_supervised(dataset_weighted_open, n_in=self.look_back, n_out=self.look_forward)
+        supervised_weighted_high = series_to_supervised(dataset_weighted_high, n_in=self.look_back, n_out=self.look_forward)
+        supervised_weighted_low = series_to_supervised(dataset_weighted_low, n_in=self.look_back, n_out=self.look_forward)
+        supervised_weighted_avg = series_to_supervised(dataset_weighted_avg, n_in=self.look_back, n_out=self.look_forward)
+        # Normalize dataset if needed
+        ori_Xy = copy.deepcopy(supervised)
+        Xy = normalize_windows(supervised)
+        feature_open = normalize_windows(supervised_open)
+        feature_high = normalize_windows(supervised_high)
+        feature_low = normalize_windows(supervised_low)
+        feature_avg = normalize_windows(supervised_avg)
+        # feature_vol = normalize_windows(supervised_vol)
+        # Feature Scaling for volume
+        from sklearn.preprocessing import MinMaxScaler
+        sc = MinMaxScaler(feature_range=(0, 1))
+        feature_vol = sc.fit_transform(supervised_vol)
+        feature_weighted_close = normalize_windows(supervised_weighted_close)
+        feature_weighted_open = normalize_windows(supervised_weighted_open)
+        feature_weighted_high = normalize_windows(supervised_weighted_high)
+        feature_weighted_low = normalize_windows(supervised_weighted_low)
+        feature_weighted_avg = normalize_windows(supervised_weighted_avg)
+
+        # Converting array of list to numpy array
+        ori_Xy = np.array(ori_Xy)
+        Xy = np.array(Xy)
+        # Spliting dataset into training and testing sets
+        self.X_ori_train, self.X_ori_test, self.y_ori_train, self.y_ori_test = train_test_split(ori_Xy, test_samples=ndays, num_forecasts=self.look_forward)
+        X_train, X_test, y_train, y_test = train_test_split(Xy, test_samples=ndays, num_forecasts=self.look_forward)
+        # Adding more features
+        feature_open_train, feature_open_test, _, _ = train_test_split(feature_open, test_samples=ndays, num_forecasts=5)
+        feature_high_train, feature_high_test, _, _ = train_test_split(feature_high, test_samples=ndays, num_forecasts=5)
+        feature_low_train, feature_low_test, _, _ = train_test_split(feature_low, test_samples=ndays, num_forecasts=5)
+        feature_avg_train, feature_avg_test, _, _ = train_test_split(feature_avg, test_samples=ndays, num_forecasts=5)
+        feature_vol_train, feature_vol_test, _, _ = train_test_split(feature_vol, test_samples=ndays, num_forecasts=5)
+        feature_weighted_close_train, feature_weighted_close_test, _, _ = train_test_split(feature_weighted_close, test_samples=ndays, num_forecasts=5)
+        feature_weighted_open_train, feature_weighted_open_test, _, _ = train_test_split(feature_weighted_open, test_samples=ndays, num_forecasts=5)
+        feature_weighted_high_train, feature_weighted_high_test, _, _ = train_test_split(feature_weighted_high, test_samples=ndays, num_forecasts=5)
+        feature_weighted_low_train, feature_weighted_low_test, _, _ = train_test_split(feature_weighted_low, test_samples=ndays, num_forecasts=5)
+        feature_weighted_avg_train, feature_weighted_avg_test, _, _ = train_test_split(feature_weighted_avg, test_samples=ndays, num_forecasts=5)
+
+        X_train = np.append(X_train, feature_open_train, axis=2)
+        X_train = np.append(X_train, feature_high_train, axis=2)
+        X_train = np.append(X_train, feature_low_train, axis=2)
+        X_train = np.append(X_train, feature_avg_train, axis=2)
+        X_train = np.append(X_train, feature_vol_train, axis=2)
+        X_train = np.append(X_train, feature_weighted_close_train, axis=2)
+        X_train = np.append(X_train, feature_weighted_open_train, axis=2)
+        X_train = np.append(X_train, feature_weighted_high_train, axis=2)
+        X_train = np.append(X_train, feature_weighted_low_train, axis=2)
+        X_train = np.append(X_train, feature_weighted_avg_train, axis=2)
+        X_test = np.append(X_test, feature_open_test, axis=2)
+        X_test = np.append(X_test, feature_high_test, axis=2)
+        X_test = np.append(X_test, feature_low_test, axis=2)
+        X_test = np.append(X_test, feature_avg_test, axis=2)
+        X_test = np.append(X_test, feature_vol_test, axis=2)
+        X_test = np.append(X_test, feature_weighted_close_test, axis=2)
+        X_test = np.append(X_test, feature_weighted_open_test, axis=2)
+        X_test = np.append(X_test, feature_weighted_high_test, axis=2)
+        X_test = np.append(X_test, feature_weighted_low_test, axis=2)
+        X_test = np.append(X_test, feature_weighted_avg_test, axis=2)
+        return X_train, y_train, X_test, y_test
+
+    def data_for_prediction(self, stock_id):
+        super(DataForStatelessModelWeighted, self)._set_look_back(stock_id)
+
+        # Taking 收盤價 開盤價 高低均價 成交量 as a predictor
+        dataset = query_close_price(self.history, stock_id)
+        dataset_open = query_open_price(self.history, int(stock_id))
+        dataset_high = query_high_price(self.history, int(stock_id))
+        dataset_low = query_low_price(self.history, int(stock_id))
+        dataset_avg = query_avg_price(self.history, int(stock_id))
+        dataset_vol = query_volume(self.history, int(stock_id))
+        dataset_weighted_close = query_weighted_close_price(self.weighted_history)
+        dataset_weighted_open = query_weighted_open_price(self.weighted_history)
+        dataset_weighted_high = query_weighted_high_price(self.weighted_history)
+        dataset_weighted_low = query_weighted_low_price(self.weighted_history)
+        dataset_weighted_avg = query_weighted_avg_price(self.weighted_history)
+        
+        # Transforming time series dataset into supervised dataset
+        supervised = series_to_supervised(dataset, n_in=self.look_back, n_out=0)
+        supervised_open = series_to_supervised(dataset_open, n_in=self.look_back, n_out=0)
+        supervised_high = series_to_supervised(dataset_high, n_in=self.look_back, n_out=0)
+        supervised_low = series_to_supervised(dataset_low, n_in=self.look_back, n_out=0)
+        supervised_avg = series_to_supervised(dataset_avg, n_in=self.look_back, n_out=0)
+        supervised_vol = series_to_supervised(dataset_vol, n_in=self.look_back, n_out=0)
+        supervised_weighted_close = series_to_supervised(dataset_weighted_close, n_in=self.look_back, n_out=self.look_forward)
+        supervised_weighted_open = series_to_supervised(dataset_weighted_open, n_in=self.look_back, n_out=self.look_forward)
+        supervised_weighted_high = series_to_supervised(dataset_weighted_high, n_in=self.look_back, n_out=self.look_forward)
+        supervised_weighted_low = series_to_supervised(dataset_weighted_low, n_in=self.look_back, n_out=self.look_forward)
+        supervised_weighted_avg = series_to_supervised(dataset_weighted_avg, n_in=self.look_back, n_out=self.look_forward)
+        # Normalize dataset if needed
+        ori_Xy = copy.deepcopy(supervised)
+        Xy = normalize_windows(supervised)
+        feature_open = normalize_windows(supervised_open)
+        feature_high = normalize_windows(supervised_high)
+        feature_low = normalize_windows(supervised_low)
+        feature_avg = normalize_windows(supervised_avg)
+        # feature_vol = normalize_windows(supervised_vol)
+        # Feature Scaling for volume
+        from sklearn.preprocessing import MinMaxScaler
+        sc = MinMaxScaler(feature_range=(0, 1))
+        feature_vol = sc.fit_transform(supervised_vol)
+        feature_weighted_close = normalize_windows(supervised_weighted_close)
+        feature_weighted_open = normalize_windows(supervised_weighted_open)
+        feature_weighted_high = normalize_windows(supervised_weighted_high)
+        feature_weighted_low = normalize_windows(supervised_weighted_low)
+        feature_weighted_avg = normalize_windows(supervised_weighted_avg)
+
+        # Converting array of list to numpy array
+        ori_Xy = np.array(ori_Xy)
+        Xy = np.array(Xy)
+        # Spliting dataset into predicting sets
+        self.X_ori_test = predict_split(ori_Xy)
+        X_test = predict_split(Xy)
+        # Adding more features
+        feature_open_test = predict_split(feature_open)
+        feature_high_test = predict_split(feature_high)
+        feature_low_test = predict_split(feature_low)
+        feature_avg_test = predict_split(feature_avg)
+        feature_vol_test = predict_split(feature_vol)
+        feature_weighted_close_test = predict_split(feature_weighted_close)
+        feature_weighted_open_test = predict_split(feature_weighted_open)
+        feature_weighted_high_test = predict_split(feature_weighted_high)
+        feature_weighted_low_test = predict_split(feature_weighted_low)
+        feature_weighted_avg_test = predict_split(feature_weighted_avg)
+        
+        X_test = np.append(X_test, feature_open_test, axis=2)
+        X_test = np.append(X_test, feature_high_test, axis=2)
+        X_test = np.append(X_test, feature_low_test, axis=2)
+        X_test = np.append(X_test, feature_avg_test, axis=2)
+        X_test = np.append(X_test, feature_vol_test, axis=2)
+        X_test = np.append(X_test, feature_weighted_close_test, axis=2)
+        X_test = np.append(X_test, feature_weighted_open_test, axis=2)
+        X_test = np.append(X_test, feature_weighted_high_test, axis=2)
+        X_test = np.append(X_test, feature_weighted_low_test, axis=2)
+        X_test = np.append(X_test, feature_weighted_avg_test, axis=2)
 
         return self.X_ori_test, X_test
 
---
+++ stateless_training_weighted.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+# 1. DataForStatelessModel
+# 2. Open, High, Low, Average, and Close Price
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from argparse import ArgumentParser
+
+from hyperopt import Trials, tpe
+from hyperas import optim
+
+import dataloader
+from util import visualize_model
+
+
+def data():
+    """
+    Data providing function:
+
+    Make sure to have every relevant import statement included here and return data as
+    used in model function below. This function is separated from model() so that hyperopt
+    won't reload data for each evaluation run.
+    """
+    # workaround to dynamically change stock_id
+    with open('.processing_stock_id', 'r') as file:
+        stock_id = file.read()
+    print('Loading dataset for {}'.format(stock_id))
+    import dataloader
+    loader = dataloader.DataForStatelessModelWeighted(int(stock_id))
+    if stock_id in ['00690', '00692', '00701', '00713']:
+        ndays = 30
+    else:
+        ndays = 240
+    X_train, y_train, X_test, y_test = loader.data_last_ndays_for_test(int(stock_id), ndays=ndays)
+    return X_train, y_train, X_test, y_test
+
+"""
+# 20180429 Open/Close/Avg
+nb_epoch = {{choice([1, 10, 50])}} => 50
+batch_size = 32
+layers = {{choice([2, 3, 4])}} => 2, 3
+output_dim = {{choice([50, 60, 90])}} => 50, 60
+optimizer = {{choice(['rmsprop', 'sgd', 'adam'])}}
+dropout = 0.2
+"""
+
+"""
+# 20180506 Open/Close/Avg/High/Low
+nb_epoch = {{choice([50, 100, 125])}} => 100, 125
+batch_size = {{choice([32, 128])}} => all
+layers = {{choice([2, 3, 4])}} => 2, 3
+output_dim = {{choice([50, 60, 70, 256])}} => 50, 60, 256
+optimizer = {{choice(['rmsprop', 'adam'])}} => all
+dropout = {{choice([0.2, 0.3])}} => all
+"""
+
+"""
+# 20180513 Open/Close/Avg/High/Low/Vol/Variable Lookback
+nb_epoch = {{choice([50, 100, 125, 150, 200])}} => 100, 150, 200
+batch_size = {{choice([32, 128])}} => all
+layers = {{choice([1, 2, 4])}} => all
+output_dim = {{choice([50, 60, 256])}} => all
+optimizer = {{choice(['rmsprop', 'adam'])}} => all
+dropout = {{choice([0.2, 0.3, 0.4])}} => all
+"""
+
+
+def model(X_train, y_train, X_test, y_test):
+    """
+    Model providing function:
+
+    Create Keras model with double curly brackets dropped-in as needed.
+    Return value has to be a valid python dictionary with two customary keys:
+        - loss: Specify a numeric evaluation metric to be minimized
+        - status: Just use STATUS_OK and see hyperopt documentation if not feasible
+    The last one is optional, though recommended, namely:
+        - model: specify the model just created so that we can later use it again.
+    """
+    # Importing the Keras libraries and packages
+    from keras.callbacks import EarlyStopping
+    # Importing the hyperopt libraries and packages
+    from hyperopt import STATUS_OK
+    from hyperas.distributions import choice
+
+    from model import create_stateless_lstm_model
+    from util import SavePredictionCallback
+
+    nb_epoch = {{choice([100, 150, 200])}}
+    batch_size = {{choice([32, 128])}}
+    layers = {{choice([1, 2, 3, 4])}}
+    output_dim = {{choice([50, 60, 256])}}
+    optimizer = {{choice(['rmsprop', 'adam'])}}
+    dropout = {{choice([0.2, 0.3, 0.4])}}
+
+    regressor = create_stateless_lstm_model(
+        X_train,
+        y_train,
+        layers=layers,
+        output_dim=output_dim,
+        optimizer=optimizer,
+        dropout=dropout)
+
+    # Defining early stopping criteria
+    earlyStopping = EarlyStopping(monitor='loss', min_delta=0.000001, patience=25, verbose=1, mode='min')
+
+    # Defining intermediate prediction result
+    predicted_prefix = 'stateless_mf_etf_predicted_epoch{}_batch{}_layers{}_output{}_dropout{}_opt{}'.format(nb_epoch, batch_size, layers, output_dim, dropout, optimizer)
+    savePrediction = SavePredictionCallback(predicted_prefix, X_test)
+
+    # Collecting callback list
+    callbacks_list = [earlyStopping, savePrediction]
+
+    # Fitting the RNN to the Training set
+    real_price = y_test
+    real_price = np.concatenate((real_price[0], np.array(real_price)[1:, -1]))
+    # np.save('stateless_real_price.npy', real_price)
+    regressor.fit(
+        X_train,
+        y_train,
+        epochs=nb_epoch,
+        batch_size=batch_size,
+        validation_data=(X_test, y_test),
+        callbacks=callbacks_list,
+        shuffle=False)
+
+    # Evaluating the model
+    score, mse = regressor.evaluate(X_test, y_test, batch_size=batch_size)
+    print('Test score: {}, mse: {}'.format(score, mse))
+
+    return {'loss': mse, 'status': STATUS_OK, 'model': regressor}
+
+
+def start_training(stock_id, trained_model):
+    # workaround to dynamically change stock_id
+    with open('.processing_stock_id', 'w') as file:
+        file.write(str(stock_id))
+    best_run, best_model = optim.minimize(
+        model=model,
+        data=data,
+        algo=tpe.suggest,
+        max_evals=5,
+        trials=Trials())
+    _, _, X_test, y_test = data()
+    print('Evalutation of best performing model for stock id {}:'.format(stock_id))
+    print(best_model.evaluate(X_test, y_test))
+    print('Best performing model for stock id {} chosen hyper-parameters:'.format(stock_id))
+    print(best_run)
+    best_model.save(trained_model)
+    # plotting best performing mode
+    if stock_id in ['00690', '00692', '00701', '00713']:
+        ndays = 30
+    else:
+        ndays = 240
+    plot_prefix = 'stateless_mf_etf_stock_{}'.format(stock_id)
+    for key, value in best_run.items():
+        plot_prefix = '_'.join((plot_prefix, str(key), str(value)))
+    loader = dataloader.DataForStatelessModelWeighted(int(stock_id))
+    visualize_model(loader, best_model, stock_id, ndays, plot_prefix)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('stock_id', help='stock id')
+    parser.add_argument('trained_model', help='output model name')
+    args = parser.parse_args()
+    print('start training process for...')
+    print('stock id: {}'.format(args.stock_id))
+    print('output model name: {}'.format(args.trained_model))
+    start_training(args.stock_id, args.trained_model)
