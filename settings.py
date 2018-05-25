1271ffe677dd0be56692d5fc963e24d5a35f7162
Jonas Cheng <jonas.cheng@gmail.com>
2018-05-25 23:35:38 +0800

    set dataset path to TBrain_Round2_DataSet_20180525

--- settings.py
+++ settings.py
@@ -4,4 +4,5 @@
 # DATASET_PATH = 'TBrain_Round2_DataSet_20180427'
 # DATASET_PATH = 'TBrain_Round2_DataSet_20180504'
 # DATASET_PATH = 'TBrain_Round2_DataSet_20180511'
-DATASET_PATH = 'TBrain_Round2_DataSet_20180518'
+# DATASET_PATH = 'TBrain_Round2_DataSet_20180518'
+DATASET_PATH = 'TBrain_Round2_DataSet_20180525'
