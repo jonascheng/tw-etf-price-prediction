d6a6d8375c61f5981b95f8b613b84912ccedf680
Jonas Cheng <jonas.cheng@gmail.com>
2018-06-15 20:50:07 +0800

    prepare for submission

--- settings.py
+++ settings.py
@@ -7,4 +7,5 @@
 # DATASET_PATH = 'TBrain_Round2_DataSet_20180518'
 # DATASET_PATH = 'TBrain_Round2_DataSet_20180525'
 # DATASET_PATH = 'TBrain_Round2_DataSet_20180601'
-DATASET_PATH = 'TBrain_Round2_DataSet_20180608'
+# DATASET_PATH = 'TBrain_Round2_DataSet_20180608'
+DATASET_PATH = 'TBrain_Round2_DataSet_20180615'
